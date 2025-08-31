3ae7d86d9362630af1271317b6d36d897e312e78
santhoshkumar0017 <msanthoshkumar217@gmail.com>
2025-08-31 17:30:55 +0530

    Delete Todolist/Backend/Todolist directory

--- Todolist/Backend/Todolist/src/main/java/com/todolist/Todolist/exception/ResourcesNotFound.java
+++
@@ -1,7 +0,0 @@
-package com.todolist.Todolist.exception;
-
-public class ResourcesNotFound extends RuntimeException {
-    public ResourcesNotFound(String message) {
-        super(message);
-    }
-}
