eb7afedd6d27ec9958e58d80d571a0a3e42275ee
santhoshkumar0017 <msanthoshkumar217@gmail.com>
2025-08-31 17:31:07 +0530

    Delete Todolist/Frontend directory

--- Todolist/Frontend/Todolist/src/Service/ServiceScript.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-const REST_API_URL = "http://localhost:8080";
-
-export const addTask=(todolist)=> axios.post(REST_API_URL+"/add",todolist)
-export const getList=() => axios.get(REST_API_URL+"/findAll")
-export const updateList=(id) => axios.put(REST_API_URL+"/update/"+id)
-export const removeList=(id) =>axios.delete(REST_API_URL+"/remove/"+id)
